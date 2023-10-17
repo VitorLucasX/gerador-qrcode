37e882e3dafdafbf5eacd45193d996900acd650c
Vitor Lucas Araujo <126624364+VitorLucasX@users.noreply.github.com>
2023-10-17 16:24:11 -0300

    commit final

---
+++ script.js
@@ -0,0 +1,26 @@
+const container = document.querySelector('.container')
+qrInput = container.querySelector('.form input')
+generateBtn = container.querySelector('.form button')
+qrImg = container.querySelector('.qr-code .img')
+
+generateBtn.addEventListener('click', function(){
+    let qrValue = qrInput.value
+    
+    if(!qrValue){
+        alert('Insira uma URL ou texto para gerar um QR Code')
+        return
+    }
+    generateBtn.innerText = 'Gerando um Qr Code...'
+    qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=%24${qrValue}`
+    
+    qrImg.addEventListener('load', function(){
+        generateBtn.innerText = 'Gerar Qr Code'
+        container.classList.add('active')
+    })
+})
+
+qrInput.addEventListener('keyup', function(){
+    if(!qrInput.value) {
+        container.classList.remove('active')
+    }
+})
